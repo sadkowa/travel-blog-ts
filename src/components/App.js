5d7c784cd94e0ee3d35e19881b8d95de12ea773b
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-29 14:35:59 +0100

    Add Global and Reset components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <header style={{ backgroundColor: 'grey', height:'300px' }}>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Category</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <h1>Blog</h1>
+    </header>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import ResetStyle from './components/global/Reset';
+import GlobalStyle from './components/global/Global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ResetStyle/>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>
 );
