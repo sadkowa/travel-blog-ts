2d7a0c1eb272ea8b956017229cc750e630d9a9bb
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-03-01 22:36:46 +0100

    Create Wrapper component

---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,9 @@
+import React from "react";
+import StyledWrapper from "./Wrapper.styled";
+
+const Wrapper = ({ children }) => {
+
+    return <StyledWrapper>{children}</StyledWrapper>
+}
+
+export default Wrapper
---
+++ src/components/Wrapper/Wrapper.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+    /* width: 80%; */
+    max-width: 1200px;
+    margin: 0 auto
+`
+
+export default StyledWrapper
