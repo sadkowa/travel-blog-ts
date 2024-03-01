2bfea6f4baa1dca1076cdb29fd2962e70effcaea
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-03-01 22:44:33 +0100

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
+import { Header } from '.'
+
 function App() {
   return (
-    <header style={{ backgroundColor: 'grey', height:'300px' }}>
-      <nav>
-        <ul>
-          <li>About</li>
-          <li>Category</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-      <h1>Blog</h1>
-    </header>
+    <Header />
   );
 }
 
 export default App;
+
