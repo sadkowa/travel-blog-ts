7c23c446e2ca9b6a55440befb351b6a9bca4e851
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-03-02 11:44:48 +0100

    Create StyledList in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,51 @@
 import React from "react";
 import StyledHeader from "./Header.styled";
-import {MainHeading, NavBar} from '../'
+import { MainHeading, NavBar, Wrapper } from '../'
 
 import { navTitles } from "../../helpers/data"
+import styled from "styled-components";
 
 const Header = () => {
 
+  const ListItem = ({ children }) => {
+    return <StyledListItem>{children}</StyledListItem>
+  }
+
     return <StyledHeader>
         <NavBar>
-        <ul>
-          {navTitles.map((item, index) => <li key={index}>{item}</li>)}
-        </ul>
+        <Wrapper>
+          <StyledList>
+            {navTitles.map((item, index) => <ListItem key={index}>{item}</ListItem>)}
+          </StyledList>
+        </Wrapper>
       </NavBar>
       <MainHeading text='Welcome at my blog!'/>
     </StyledHeader>
 }
 
 export default Header
+
+
+const StyledList = styled.ul`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  height: 100%;
+  margin: 0 30px;
+  list-style: none;
+  font-size: 1.5rem;
+`
+
+const StyledListItem = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 100%;
+  transition: 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgray;
+  }
+`
