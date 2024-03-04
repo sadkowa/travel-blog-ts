3328cb65655c198866eff9d49bc7bf8c0f5c4fb8
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-03-04 12:58:15 +0100

    Improve Routes in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import { Footer, Header, NotFound, Wrapper } from '.'
 import { About, Blog, Contact, ArticlePage } from '../views';
 import * as prismicH from '@prismicio/helpers';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom/'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom/'
 
 import { useAllPrismicDocumentsByType } from "@prismicio/react";
 
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   const [posts] = useAllPrismicDocumentsByType('post');
@@ -18,27 +19,35 @@ function App() {
     <Router>
       <Header />
       <Wrapper>
+        <ScrollToTop />
         <Switch>
           <Route exact path='/'>
             <Blog posts={posts} />
           </Route>
-          <Route path='/page/:id'>
+          <Route exact path='/page/:id'>
             <Blog posts={posts} />
           </Route>
           <Route exact path='/about'>
             <About />
           </Route>
-          <Route path='/contact'>
+          <Route exact path='/contact'>
             <Contact />
           </Route>
-          <Route path='/article/:category/:slug'>
+          <Route exact path='/article/:category'>
             <ArticlePage posts={posts}/>
           </Route>
-          <Route><NotFound /></Route>
+          <Route exact path='/article/:category/:slug'>
+            <ArticlePage posts={posts} />
+          </Route>
+          <Route path='/404.html'>
+            <NotFound />
+          </Route>
+          <Route>
+            <Redirect to='/404.html' />
+          </Route>
         </Switch>
       </Wrapper>
       <Footer />
-
     </Router>
   );
 }
