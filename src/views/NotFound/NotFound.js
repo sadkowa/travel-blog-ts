0d6f0a66dfbd7c87e3d87bbb7dfaa7686ebc9c1e
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-03-05 15:45:54 +0100

    Move NotFound to views folder

---
+++ src/views/NotFound/NotFound.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import StyledNotFound from './NotFound.styled';
+import { StyledText } from '../../components/Article/Article';
+import styled from 'styled-components';
+
+const NotFound = () => {
+
+    const styles = {
+        color: '#a07849',
+    }
+    return (
+        <StyledNotFound>
+            <StyledText>The provided page does not exist! <br />Please return to <Link style={styles} to="/"><StyledSpan>Home</StyledSpan></Link>.</StyledText>
+
+        </StyledNotFound>
+    );
+}
+
+export default NotFound;
+
+const StyledSpan = styled.span`
+    padding-left: 10px;
+    font-weight: 500;
+    font-size: 1.5rem;
+    letter-spacing: 2px;
+
+    &:hover {
+        color: #87745e;
+    }
+`
